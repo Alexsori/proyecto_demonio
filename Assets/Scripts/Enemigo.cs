29a9d4afbe04b4ace47557ca4ada6ec2988553a8
Heidism15 <heidism1508@gmail.com>
2024-01-09 10:43:28 +0100

    enemigo

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    public string enemyName; //El nombre del enemigo, usado para estadisticas de cantidad de Kills
+    public float healthPoints; //Cantidad de puntos de vida de nuestro enemigo
+    public float speed; //velocidad al caminar
+    public float knockbackForceX; //fuerza que se apliacara horizontalmente al recibir golpe
+    public float knockbackForceY; //fuerza que se aplicara verticalmente al recibir golpe
+    public float damageToGive; //daño que hara al jugador
+}
