1b658b66284d73d255814e8938e694663a882d41
VelascoCarlos <gabumon09@gmail.com>
2024-01-09 09:27:27 +0100

    Tutorial2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed, jumpHeight;
+    float velX, velY;
+    Rigidbody2D rb;
+    public Transform groundcheck;
+    public bool isGrounded;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;  
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(groundcheck.position, groundCheckRadius, whatIsGround);
+
+        flipCharacter();
+
+
+    }
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    public void Jump()
+    {
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }    
+
         
+    }
+
+    
+        
+    
+
+
+
+    public void Movement()
+    {
+        velX = Input.GetAxisRaw("Horizontal");
+        velY = rb.velocity.y;
+
+        rb.velocity = new Vector2(velX * speed, velY);
+    }
+
+    public void flipCharacter()
+    {
+        if (rb.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 }
