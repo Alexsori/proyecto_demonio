7702ab75d4fa781ae652f829feae832098db5d74
Heidism15 <heidism1508@gmail.com>
2024-01-16 01:45:13 +0100

    Flip hecho por fin

--- Assets/Scripts/EnemyScripts/Flip.cs
+++ Assets/Scripts/EnemyScripts/Flip.cs
@@ -5,26 +5,35 @@ using UnityEngine;
 public class Flip : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
+    private Vector3 previousPosition;
 
     // On awake, get the sprite renderer component
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        previousPosition = transform.position;
     }
 
     // On update, check the direction of movement
     private void Update()
     {
-        // If the enemy is moving to the left, don't flip the sprite
-        if (transform.localScale.x < 0)
-        {
-            spriteRenderer.flipX = false;
-        }
+        // Get the current position of the enemy
+        Vector3 currentPosition = transform.position;
 
-        // If the enemy is moving to the right, flip the sprite
-        else
+        // Calculate the distance between the current position and the previous position
+        float distance = Vector3.Distance(previousPosition, currentPosition);
+
+        // If the distance is greater than a threshold, then the enemy has changed direction
+        if (distance > 0.1f)
         {
-            spriteRenderer.flipX = true;
+            // Get the direction of movement
+            Vector3 direction = currentPosition - previousPosition;
+
+            // Set the flip state
+            spriteRenderer.flipX = direction.x < 0;
+
+            // Set the previous position to the current position
+            previousPosition = currentPosition;
         }
     }
 }
