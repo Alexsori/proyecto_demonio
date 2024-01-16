b1fb74ab9e7da047566068a4664b7a7331ee68b4
DiazAlicia <151826483+DiazAlicia@users.noreply.github.com>
2024-01-16 17:07:36 +0100

    Collider Lobo arreglado

--- Assets/Scripts/Player/TransformacionSprite.cs
+++ Assets/Scripts/Player/TransformacionSprite.cs
@@ -9,12 +9,14 @@ public class TransformacionSprite : MonoBehaviour
     public float duracionTransformacion = 5f;
     private SpriteRenderer spriteRenderer;
     private Sprite spriteOriginal;
+    private CapsuleCollider2D capsuleCollider;
     private bool enTransformacion = false;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteOriginal = spriteRenderer.sprite;
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
@@ -32,12 +34,18 @@ public class TransformacionSprite : MonoBehaviour
     {
         enTransformacion = true;
 
+        Vector2 colliderOriginalSize = capsuleCollider.size;
+
 
         Animator animator = GetComponent<Animator>();
         animator.runtimeAnimatorController = animatorControllerNuevo;
         spriteRenderer.flipX = true;
+        capsuleCollider.size = new Vector2(colliderOriginalSize.x + 0.20f, colliderOriginalSize.y - 0.60f);
+        capsuleCollider.direction = CapsuleDirection2D.Horizontal;
         yield return new WaitForSeconds(duracionTransformacion);
-        animator.runtimeAnimatorController = animatorControllerInicial; 
+        animator.runtimeAnimatorController = animatorControllerInicial;
+        capsuleCollider.size = colliderOriginalSize;
+        capsuleCollider.direction = CapsuleDirection2D.Vertical;
         spriteRenderer.flipX = false;
         yield return new WaitForSeconds(2f);
         spriteRenderer.sprite = spriteOriginal;
